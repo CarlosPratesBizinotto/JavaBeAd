3955f95f8396e7847c94a3d892f5cb75e7500005
CarlosViniciusBizinottoPrates <viniciusbizinotto@gmail.com>
2022-10-04 22:01:45 -0300

    Metodos

--- Indroction/src/MinhaClass.java
+++ Indroction/src/MinhaClass.java
@@ -1,9 +1,21 @@
 public class MinhaClass {
 
     public static void main (String [] args){
-        
-     System.out.println("Teste processo para ser processado.");
+       
+       String primeiroNome = "Carlos";
+       String segundoNome = "Prates";
 
-    }
+       String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+       System.out.print(nomeCompleto);
+    
+     }
+
+     public static String nomeCompleto (String primeiroNome, String segundoNome){
+       return primeiroNome.concat(" ").concat(segundoNome);
+     }
+       
+
+    
     
 }
