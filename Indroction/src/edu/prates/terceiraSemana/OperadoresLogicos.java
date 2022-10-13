11c0b5f3adca0a70d6079acbe62577e2eb90115f
CarlosViniciusBizinottoPrates <viniciusbizinotto@gmail.com>
2022-10-12 23:11:15 -0300

    Operadores

---
+++ Indroction/src/edu/prates/terceiraSemana/OperadoresLogicos.java
@@ -0,0 +1,19 @@
+package edu.prates.terceiraSemana;
+
+public class OperadoresLogicos {
+    public static void main(String[]args){
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras?");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Um das condições é vardadeira");
+        }
+        
+        System.out.println("Fim");
+    }
+    
+}
